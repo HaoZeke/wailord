be41226674ac0778b6283814a866f7cd03ef35ba
Rohit Goswami <rgoswami@ieee.org>
2020-10-11 12:51:57 +0000

    nix: Init lorri and niv

---
+++ shell.nix
@@ -0,0 +1,7 @@
+let
+  sources = import ./nix/sources.nix;
+  pkgs = import sources.nixpkgs { };
+  customPython = pkgs.poetry2nix.mkPoetryEnv { projectDir = ./.; };
+in pkgs.mkShell {
+  buildInputs = with pkgs; [ customPython rake darkhttpd bashInteractive ];
+}
