010954ef12bc1b3db12c615990df4e3f175e9451
Rohit Goswami <rgoswami@ieee.org>
2020-11-03 23:15:37 +0000

    utils: More comments

--- wailord/utils.py
+++ wailord/utils.py
@@ -3,5 +3,6 @@ from pathlib import Path
 def get_project_root() -> Path:
     """
     A helper to obtain the project root path
+    From here: https://stackoverflow.com/a/53465812/1895378
     """
     return Path(__file__).parent.parent
