b66fa06ac852e1e04634c47649c2ee904dcbccd4
Rohit Goswami <rgoswami@ieee.org>
2020-11-16 06:50:21 +0000

    utils: Add and document repkey

--- wailord/utils.py
+++ wailord/utils.py
@@ -7,3 +7,19 @@ def get_project_root() -> Path:
     From here: https://stackoverflow.com/a/53465812/1895378
     """
     return Path(__file__).parent.parent
+
+
+def repkey(fname, repobj):
+    """
+    A helper function to deal with replacements in files
+    repobj: A dictionary with "prev" and "to" keys
+    """
+    assert len(repobj["prev"]) == len(
+        repobj["to"]
+    ), "The replacement dictionary must contain as many targets as values"
+    with open(fname, "r") as f:
+        fInp = f.read()
+    for p, t in zip(repobj["prev"], repobj["to"]):
+        fInp = fInp.replace(p, t)
+    with open(fname, "w") as o:
+        o.write(fInp)
