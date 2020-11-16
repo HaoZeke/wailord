12a81c2eae3dd96a0739267fc374a5027edf443f
Rohit Goswami <rgoswami@ieee.org>
2020-11-16 04:50:34 +0000

    hooks: Update to allow more flexibility

--- templates/basicExperiment/hooks/post_gen_project.py
+++ templates/basicExperiment/hooks/post_gen_project.py
@@ -1,6 +1,7 @@
 """
 This uses wailord functions to generate the experimental structure
 """
+import shutil
 from pathlib import Path
 
 import wailord.io as waio
@@ -9,6 +10,10 @@ from wailord.utils import get_project_root
 CUR = Path().absolute()
 
 print("{{cookiecutter.project_slug}}")
+if "{{cookiecutter.orca_yml}}" != "orca.yml":
+    shutil.copy("{{cookiecutter.orca_yml}}", "orca.yml")
+if "{{cookiecutter.inp_xyz}}" != "inp.xyz":
+    shutil.copy("{{cookiecutter.inp_xyz}}", "inp.xyz")
 ymlt = waio.inp.inpParser(CUR / "orca.yml")
 ymlt.parse_yml()
 ymlt.gendir_qc(basename=Path("{{cookiecutter.project_name}}"))
