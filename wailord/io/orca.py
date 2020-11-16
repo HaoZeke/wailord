baa2877416cde2a4d9a9b99eddf14db2e27d789f
Rohit Goswami <rgoswami@ieee.org>
2020-11-16 08:08:23 +0000

    orca: Finalize helpers

--- wailord/io/orca.py
+++ wailord/io/orca.py
@@ -22,6 +22,19 @@ Todo:
 
 """
 
+
+import re, itertools, sys, os
+from pathlib import Path
+from collections import namedtuple
+
+from pint import UnitRegistry
+import numpy as np
+import pandas as pd
+import vg
+
+ureg = UnitRegistry()
+Q_ = ureg.Quantity
+
 inpcart = namedtuple("inpcart", "atype x y z")
 orcaout = namedtuple("orcaout", "final_energy fGeom basis filename system")
 
@@ -84,3 +97,45 @@ def get_e(orcaoutdat, basis, system):
     return orcaoutdat[
         (orcaoutdat.basis.isin([basis]) & (orcaoutdat.system.isin([system])))
     ]["final_energy"].to_list()[0]
+
+
+def getBL(dat, x, y, z, indi=[0, 1]):
+    v1 = np.array(
+        [dat.x[indi[0]].magnitude, dat.y[indi[0]].magnitude, dat.z[indi[0]].magnitude]
+    )
+    v2 = np.array(
+        [dat.x[indi[1]].magnitude, dat.y[indi[1]].magnitude, dat.z[indi[1]].magnitude]
+    )
+    return Q_(vg.euclidean_distance(v1, v2), x[indi[0]].units)
+
+
+def getBA(dat, x, y, z, indi=[0, 1, 2]):
+    v1 = np.array(
+        [dat.x[indi[0]].magnitude, dat.y[indi[0]].magnitude, dat.z[indi[0]].magnitude]
+    )
+    v2 = np.array(
+        [dat.x[indi[1]].magnitude, dat.y[indi[1]].magnitude, dat.z[indi[1]].magnitude]
+    )
+    v3 = np.array(
+        [dat.x[indi[2]].magnitude, dat.y[indi[2]].magnitude, dat.z[indi[2]].magnitude]
+    )
+    return Q_(vg.angle(v1, v2, v3), "degrees")
+
+
+def genEnergySet(rootdir, latex=False, full=False):
+    """Takes in a Path object, and optionally returns a TeX table or a full dataset with the filenames and geometries"""
+    outs = []
+    for root, dirs, files in os.walk(rootdir.resolve()):
+        for filename in files:
+            if "out" in filename and "slurm" not in filename:
+                outs.append(parseOut(f"{root}/{filename}"))
+    outdat = pd.DataFrame(data=outs)
+    if latex == True:
+        return outdat.drop(["filename", "fGeom"], axis=1).to_latex(
+            caption="Calculated systems at both basis sets", index=False
+        )
+    elif full == False:
+        outdat = pd.DataFrame(data=outs).drop(["filename", "fGeom"], axis=1)
+    else:
+        pass
+    return outdat
