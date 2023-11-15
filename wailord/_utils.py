d73628287de89d822a29e001e6adc542d63f5c0e
Rohit Goswami <rog32@hi.is>
2023-11-15 23:37:29 +0000

    ENH: Add DotDict

--- wailord/_utils.py
+++ wailord/_utils.py
@@ -24,3 +24,45 @@ def repkey(fname, repobj):
     with open(fname, "w") as o:
         o.write(fInp)
 
+class DotDict(dict):
+    """
+    Modified dictionary class for accessing key:val via dot notation.
+    Inspired by the MIT licensed (defunct) Konfik library
+    """
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for key in self.keys():
+            if isinstance(self[key], dict):
+                self[key] = DotDict(self[key])
+
+    def __getitem__(self, key):
+        if isinstance(key, str):
+            try:
+                return super().__getitem__(key)
+            except KeyError:
+                raise MissingVariableError(f"No such variable '{key}' exists") from None
+        else:
+            raise TypeError("Key must be a string")
+
+    def __setitem__(self, key, value):
+        if isinstance(value, dict) and not isinstance(value, DotDict):
+            value = DotDict(value)
+        super().__setitem__(key, value)
+
+    def __getattr__(self, key):
+        if key.startswith('__') and key.endswith('__'):
+            return super().__getattr__(key)
+        return self.__getitem__(key)
+
+    def __setattr__(self, key, value):
+        if key.startswith('__') and key.endswith('__'):
+            super().__setattr__(key, value)
+        else:
+            self.__setitem__(key, value)
+
+    def __delattr__(self, key):
+        if key in self:
+            del self[key]
+        else:
+            super().__delattr__(key)
